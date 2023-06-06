437ef0a55b0390db61cd359f2dc46dcd5a1420d1
The-Caterpillar <saraswati99tiwari@gmail.com>
2023-06-06 19:18:30 +0530

    More work

---
+++ Lecture-8.py
@@ -0,0 +1,38 @@
+print("\n ")
+
+print("OUTPUT: ")
+
+dic = {'Saraswati':75,'Priyanka':70,'Prakhar':85}
+print(dic)
+
+l = dic.keys()
+print(l)
+
+m=dic.values()
+print(m)
+
+dic['T'] = 100
+
+if('Saraswati' in dic):
+	print("YES")
+else:
+	print("NO")
+
+for i in dic:
+	print(i,dic[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n ")
