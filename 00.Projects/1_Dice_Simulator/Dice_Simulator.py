e52e6ccaa6f05769f8fe18d23ee2f56d64e10e2c
The-Caterpillar <saraswati99tiwari@gmail.com>
2023-07-09 14:10:27 +0530

    Python Desktop Application
    A dice simulator using Pyhton

---
+++ 00.Projects/1_Dice_Simulator/Dice_Simulator.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import random
+
+window = tk.Tk()
+window.geometry("600x400")
+window.title("Dice Roll")
+
+dice = ["1.png","2.png","3.png",'4.png','5.png','6.png']
+image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+image2 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+label1 = tk.Label(window,image=image1)
+label2 = tk.Label(window,image=image2)
+
+label1.image = image1
+label2.image = image2
+
+label1.place(x=60,y=100)
+label2.place(x=320,y=100)
+
+def dice_roll():
+    image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    label1.configure(image=image1)
+    label1.image = image1
+
+    image2 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    label2.configure(image=image2)
+    label2.image = image2
+
+button = tk.Button(window,text="ROLL", bg="red", fg="white", font="Times 20 bold", command=dice_roll)
+button.place(x = 250, y=10)
+
+window.mainloop()
